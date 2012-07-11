a9f00e5516ff9b219f9b84367dc75dd789c4fd8c
Kamesh Sampath <kamesh.sampath@hotmail.com>
2012-07-11 10:45:23 +0530

    IDE-116 Fixed the Selection issue with checking for portlet names.

--- eclipse/portal/plugins/com.liferay.ide.eclipse.portlet.ui/src/com/liferay/ide/eclipse/portlet/ui/navigator/actions/OpenPortletResourceAction.java
+++ eclipse/portal/plugins/com.liferay.ide.eclipse.portlet.ui/src/com/liferay/ide/eclipse/portlet/ui/navigator/actions/OpenPortletResourceAction.java
@@ -15,6 +15,7 @@
 
 package com.liferay.ide.eclipse.portlet.ui.navigator.actions;
 
+import com.liferay.ide.eclipse.portlet.core.model.IPortlet;
 import com.liferay.ide.eclipse.portlet.ui.navigator.PortletNavigatorNode;
 import com.liferay.ide.eclipse.ui.navigator.LiferayIDENavigatorNode;
 
@@ -23,12 +24,14 @@ import org.eclipse.core.runtime.content.IContentType;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.sapphire.modeling.IModelElement;
+import org.eclipse.sapphire.ui.SapphireEditor;
 import org.eclipse.sapphire.ui.form.editors.masterdetails.MasterDetailsContentNode;
 import org.eclipse.sapphire.ui.form.editors.masterdetails.MasterDetailsContentOutline;
 import org.eclipse.sapphire.ui.form.editors.masterdetails.MasterDetailsEditorPage;
 import org.eclipse.sapphire.ui.swt.xml.editor.SapphireEditorForXml;
 import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IEditorRegistry;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
@@ -42,9 +45,16 @@ import org.eclipse.ui.part.FileEditorInput;
 public class OpenPortletResourceAction extends BaseSelectionListenerAction
 {
 
+    /**
+     * 
+     */
+    private static final String PORTLETS_NODE_LABEL = "Portlets";
+
     private static final String ACTION_MESSAGE = "Open";
 
     protected LiferayIDENavigatorNode selectedNode;
+
+    protected IEditorPart editorPart;
 
     /**
      * @param text
@@ -63,9 +73,21 @@ public class OpenPortletResourceAction extends BaseSelectionListenerAction
     @Override
     protected boolean updateSelection( IStructuredSelection selection )
     {
+
         if( selection.size() == 1 )
         {
             this.selectedNode = (LiferayIDENavigatorNode) selection.getFirstElement();
+
+            if( editorPart == null )
+            {
+                initEditorPart();
+            }
+
+            if( editorPart != null && this.selectedNode instanceof PortletNavigatorNode )
+            {
+                selectAndRevealItem( editorPart );
+            }
+
             return true;
         }
 
@@ -81,57 +103,116 @@ public class OpenPortletResourceAction extends BaseSelectionListenerAction
     {
         if( isEnabled() )
         {
-
-            IFile file = null;
-
-            if( this.selectedNode.getResource() == null )
-            {
-                file = this.selectedNode.getParent().getResource();
-            }
-            else
-            {
-                file = this.selectedNode.getResource();
-            }
+            IFile file = initEditorPart();
 
             if( file != null && file.exists() )
             {
-                IEditorPart editorPart = openEditor( file );
+                editorPart = openEditor( file );
+
                 if( editorPart != null && this.selectedNode instanceof PortletNavigatorNode )
                 {
-                    // TODO: need to work on to fix to reveal the selected node
-                    SapphireEditorForXml editor = (SapphireEditorForXml) editorPart;
-                    PortletNavigatorNode portletNavigatorNode = (PortletNavigatorNode) this.selectedNode;
-                    IModelElement modelElement = portletNavigatorNode.getModel();
-                    if( modelElement != null )
-                    {
-                        MasterDetailsEditorPage mdepDetailsEditorPage =
-                            (MasterDetailsEditorPage) editor.getActivePageInstance();
-                        if( mdepDetailsEditorPage != null )
-                        {
-                            MasterDetailsContentOutline contentOutline = mdepDetailsEditorPage.outline();
-                            MasterDetailsContentNode rootNode = contentOutline.getRoot();
-
-                            if( rootNode != null )
-                            {
-                                MasterDetailsContentNode portletAppNode = rootNode.getChildNodes().get( 0 );
-                                if( portletAppNode != null )
-                                {
-                                   MasterDetailsContentNode selectedPortletNode =
-                                        portletAppNode.findNodeByModelElement(
-                                            modelElement );
-
-                                    if( selectedPortletNode != null )
-                                    {
-                                        selectedPortletNode.setExpanded( true );
-                                    }
-                                }
-                            }
-                        }
-
-                    }
+                    selectAndRevealItem( editorPart );
                 }
             }
         }
+    }
+
+    /**
+     * @param editor
+     *            TODO: need to work on to fix to reveal the selected node
+     */
+    protected void selectAndRevealItem( IEditorPart editorPart )
+    {
+
+        if( this.editorPart instanceof SapphireEditor )
+        {
+
+            SapphireEditorForXml editor = (SapphireEditorForXml) editorPart;
+
+            PortletNavigatorNode portletNavigatorNode = (PortletNavigatorNode) this.selectedNode;
+            IModelElement selectedModelElement = portletNavigatorNode.getModel();
+
+            if( selectedModelElement != null )
+            {
+                MasterDetailsEditorPage mdepDetailsEditorPage =
+                    (MasterDetailsEditorPage) editor.getActivePageInstance();
+                if( mdepDetailsEditorPage != null )
+                {
+                    MasterDetailsContentOutline contentOutline = mdepDetailsEditorPage.outline();
+                    MasterDetailsContentNode rootNode = contentOutline.getRoot();
+
+                    if( rootNode != null )
+                    {
+                        // debugNodes( rootNode );
+                        MasterDetailsContentNode portletAppNode = rootNode.getChildNodes().get( 0 );
+                        // debugNodes( portletAppNode );
+                        MasterDetailsContentNode portletsNode =
+                            portletAppNode.getChildNodeByLabel( PORTLETS_NODE_LABEL );
+                        // TODO: Performance Check ???, cant we not have the shared model ?
+                        if( portletsNode != null )
+                        {
+                            if( selectedModelElement instanceof IPortlet )
+                            {
+                                IPortlet selectedPortlet = (IPortlet) selectedModelElement;
+                                for( MasterDetailsContentNode childNode : portletsNode.getChildNodes() )
+                                {
+                                    String selectedPortletName = selectedPortlet.getPortletName().getContent();
+                                    if( childNode.getModelElement() instanceof IPortlet )
+                                    {
+                                        IPortlet mpContentNodePortlet = (IPortlet) childNode.getModelElement();
+                                        String mpContentNodePortletName =
+                                            mpContentNodePortlet.getPortletName().getContent();
+
+                                        if( selectedPortletName.equals( mpContentNodePortletName ) )
+                                        {
+                                            childNode.select();
+                                            childNode.setExpanded( true );
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    /**
+     * 
+     */
+    protected IFile initEditorPart()
+    {
+        IFile file = null;
+
+        if( this.selectedNode.getResource() == null )
+        {
+            file = this.selectedNode.getParent().getResource();
+        }
+        else
+        {
+            file = this.selectedNode.getResource();
+        }
+
+        // Check to see if the editor part is already open
+        if( editorPart == null && file != null )
+        {
+            IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+            IEditorReference[] editorReferences = page.getEditorReferences();
+            for( IEditorReference iEditorReference : editorReferences )
+            {
+                if( file.getName().equals( iEditorReference.getName() ) )
+                {
+                    this.editorPart = iEditorReference.getEditor( false );
+                }
+                // TODO check the same for Liferay Portlet XMl Editor
+            }
+        }
+
+        return file;
     }
 
     protected IEditorPart openEditor( IFile file )
