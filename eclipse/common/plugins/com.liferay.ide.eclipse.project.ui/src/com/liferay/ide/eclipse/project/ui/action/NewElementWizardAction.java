49a22465f093501168e853d7f6783ca6be7e7c81
Cindy Li <cindy.li@liferay.com>
2012-09-25 16:07:37 +0800

    IDE-63-3

--- eclipse/common/plugins/com.liferay.ide.eclipse.project.ui/src/com/liferay/ide/eclipse/project/ui/action/NewElementWizardAction.java
+++ eclipse/common/plugins/com.liferay.ide.eclipse.project.ui/src/com/liferay/ide/eclipse/project/ui/action/NewElementWizardAction.java
@@ -1,12 +1,16 @@
 package com.liferay.ide.eclipse.project.ui.action;
 
 import com.liferay.ide.eclipse.core.util.CoreUtil;
+import com.liferay.ide.eclipse.project.core.util.ProjectUtil;
+
+import java.util.Set;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
 
 
 public class NewElementWizardAction extends NewWizardAction
@@ -22,15 +26,20 @@ public class NewElementWizardAction extends NewWizardAction
 
         for( IProject project : projects )
         {
+            Set<IProjectFacetVersion> facets = ProjectUtil.getFacetedProject( project ).getProjectFacets();
+
             if( validProjectTypes != null )
             {
                 String[] validTypes = validProjectTypes.split( "," );
                 for( String validProjectType : validTypes )
                 {
-                    if( project.getName().contains( validProjectType ) )
+                    for( IProjectFacetVersion facet : facets )
                     {
-                        hasValidProjectTypes = true;
-                        break;
+                        String id = facet.getProjectFacet().getId();
+                        if(id.startsWith( "liferay." ) && id.equals( "liferay." + validProjectType )) {
+                            hasValidProjectTypes = true;
+                            break;
+                        }
                     }
                 }
             }
