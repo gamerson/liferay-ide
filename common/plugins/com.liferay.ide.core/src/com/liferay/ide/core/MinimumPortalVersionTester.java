1359d7843e54f28a70522625de4eb01d004325ad
Cindy Li <cindy.li@liferay.com>
2013-02-01 16:47:31 +0800

    IDE-804

---
+++ common/plugins/com.liferay.ide.core/src/com/liferay/ide/core/MinimumPortalVersionTester.java
@@ -0,0 +1,53 @@
+package com.liferay.ide.core;
+
+import com.liferay.ide.core.util.CoreUtil;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.osgi.framework.Version;
+
+
+/**
+ * @author Cindy Li
+ */
+public class MinimumPortalVersionTester extends PropertyTester
+{
+
+    public boolean test( Object receiver, String property, Object[] args, Object expectedValue )
+    {
+        IProject project = null;
+
+        if( receiver instanceof IProject )
+        {
+            project = ((IProject) receiver );
+        }
+        else if( receiver instanceof IFile )
+        {
+            project = ((IFile) receiver).getProject();
+        }
+
+        try
+        {
+            final ILiferayProject lProject = LiferayCore.create( project );
+
+            if( lProject != null )
+            {
+                final Version version = new Version( lProject.getPortalVersion() );
+                Version minimumRequiredPortalVersion = new Version( (String) args[0] );
+
+                if( CoreUtil.compareVersions( version, minimumRequiredPortalVersion ) >= 0 )
+                {
+                    return true;
+                }
+            }
+        }
+        catch( Exception e )
+        {
+            LiferayCore.logError( "Could not get liferay runtime.", e ); //$NON-NLS-1$
+        }
+
+        return false;
+    }
+
+}
