efe63e510d73b8359f3789338fe99d3ca1ceef14
Simon Jiang <simon.jiang@liferay.com>
2014-05-29 21:09:03 +0800

    IDE-1397 kepler progress is too slow to restart server for deploy ext

--- tools/plugins/com.liferay.ide.server.tomcat.core/src/com/liferay/ide/server/tomcat/core/TomcatExtPluginPublisher.java
+++ tools/plugins/com.liferay.ide.server.tomcat.core/src/com/liferay/ide/server/tomcat/core/TomcatExtPluginPublisher.java
@@ -12,15 +12,7 @@
  * details.
  *
  *******************************************************************************/
-
 package com.liferay.ide.server.tomcat.core;
-
-import com.liferay.ide.project.core.util.ProjectUtil;
-import com.liferay.ide.sdk.core.SDK;
-import com.liferay.ide.sdk.core.SDKUtil;
-import com.liferay.ide.server.core.AbstractPluginPublisher;
-import com.liferay.ide.server.tomcat.core.util.LiferayTomcatUtil;
-import com.liferay.ide.server.util.ServerUtil;
 
 import java.util.Map;
 
@@ -29,14 +21,23 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.wst.server.core.IModule;
 import org.eclipse.wst.server.core.IServer;
 import org.eclipse.wst.server.core.model.IModuleResourceDelta;
 import org.eclipse.wst.server.core.model.ServerBehaviourDelegate;
 
+import com.liferay.ide.project.core.util.ProjectUtil;
+import com.liferay.ide.sdk.core.SDK;
+import com.liferay.ide.sdk.core.SDKUtil;
+import com.liferay.ide.server.core.AbstractPluginPublisher;
+import com.liferay.ide.server.tomcat.core.util.LiferayTomcatUtil;
+import com.liferay.ide.server.util.ServerUtil;
+
 /**
  * @author Greg Amerson
+ * @author Simon Jiang
  */
 public class TomcatExtPluginPublisher extends AbstractPluginPublisher
 {
@@ -111,7 +112,7 @@ public class TomcatExtPluginPublisher extends AbstractPluginPublisher
         return true;
     }
 
-    protected void addExtModule( ServerBehaviourDelegate delegate, IModule module, IProgressMonitor monitor )
+    protected void addExtModule( final ServerBehaviourDelegate delegate, IModule module, final IProgressMonitor monitor )
         throws CoreException
     {
 
@@ -126,7 +127,7 @@ public class TomcatExtPluginPublisher extends AbstractPluginPublisher
                 LiferayTomcatPlugin.createErrorStatus( "No SDK for project configured. Could not deploy ext module" ) ); //$NON-NLS-1$
         }
 
-        String mode =
+        final String mode =
             delegate.getServer().getServerState() == IServer.STATE_STARTED ? delegate.getServer().getMode() : null;
 
         if( mode != null )
@@ -142,7 +143,24 @@ public class TomcatExtPluginPublisher extends AbstractPluginPublisher
 
         if( mode != null )
         {
-            delegate.getServer().start( mode, monitor );
+            new ServerJob( delegate.getServer(), "Starting Liferay server after ext plugin deploy" )
+            {
+
+                @Override
+                protected IStatus run( IProgressMonitor monitor )
+                {
+                    try
+                    {
+                        delegate.getServer().start( mode, monitor );
+                    }
+
+                    catch( CoreException e )
+                    {
+                        LiferayTomcatPlugin.logError( "Failed to restart server for ext module.", e ); //$NON-NLS-1$
+                    }
+                    return Status.OK_STATUS;
+                }
+            }.schedule();
         }
     }
 
@@ -172,6 +190,28 @@ public class TomcatExtPluginPublisher extends AbstractPluginPublisher
         static
         {
             initializeMessages( TomcatExtPluginPublisher.class.getName(), Msgs.class );
+        }
+    }
+    
+    private static abstract class ServerJob extends Job
+    {
+        private IServer server;
+
+        public ServerJob( IServer server, String name )
+        {
+            super( name );
+            this.server = server;
+        }
+
+        public boolean belongsTo( Object family )
+        {
+            return org.eclipse.wst.server.core.ServerUtil.SERVER_JOB_FAMILY.equals( family );
+        }
+
+        @SuppressWarnings( "unused" )
+        public IServer getServer()
+        {
+            return this.server;
         }
     }
 }
