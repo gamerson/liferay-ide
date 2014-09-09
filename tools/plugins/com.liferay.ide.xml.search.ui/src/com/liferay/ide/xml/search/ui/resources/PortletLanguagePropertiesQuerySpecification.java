7f6214db853e7299e9e33e74029ba9f4ac569c7a
Terry Jia <terry.jia@liferay.com>
2014-09-09 17:41:20 +0800

    IDE-1567 Add contentAssist and warming message for liferay-ui:message tag

---
+++ tools/plugins/com.liferay.ide.xml.search.ui/src/com/liferay/ide/xml/search/ui/resources/PortletLanguagePropertiesQuerySpecification.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+
+package com.liferay.ide.xml.search.ui.resources;
+
+import org.eclipse.wst.xml.search.core.properties.IPropertiesRequestor;
+import org.eclipse.wst.xml.search.core.properties.IPropertiesRequestorProvider;
+
+/**
+ * @author Terry Jia
+ */
+public class PortletLanguagePropertiesQuerySpecification implements IPropertiesRequestorProvider
+{
+
+    @Override
+    public IPropertiesRequestor getRequestor()
+    {
+        return PortletLanguagePropertiesRequestor.INSTANCE;
+    }
+
+}
