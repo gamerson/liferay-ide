7562bc2fc408298187abe586a5b00015d55a550a
Terry Jia <terry.jia@liferay.com>
2015-01-07 16:30:20 +0800

    IDE-1855 Add the UI tests

--- tools/tests/com.liferay.ide.xml.search.ui.tests/src/com/liferay/ide/xml/search/ui/tests/JSPFileTests.java
+++ tools/tests/com.liferay.ide.xml.search.ui.tests/src/com/liferay/ide/xml/search/ui/tests/JSPFileTests.java
@@ -16,9 +16,11 @@
 package com.liferay.ide.xml.search.ui.tests;
 
 import static com.liferay.ide.xml.search.ui.tests.XmlSearchTestsUtils.buildAndValidate;
+import static com.liferay.ide.xml.search.ui.tests.XmlSearchTestsUtils.checkMarkerByMessage;
 import static com.liferay.ide.xml.search.ui.tests.XmlSearchTestsUtils.deleteOtherProjects;
 import static com.liferay.ide.xml.search.ui.tests.XmlSearchTestsUtils.setAttrValue;
 import static com.liferay.ide.xml.search.ui.tests.XmlSearchTestsUtils.verifyQuickFix;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import com.liferay.ide.core.util.CoreUtil;
@@ -26,12 +28,14 @@ import com.liferay.ide.xml.search.ui.AddResourceKeyMarkerResolution;
 import com.liferay.ide.xml.search.ui.XMLSearchConstants;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.junit.Test;
-
 
 /**
  * @author Kuo Zhang
+ * @author Terry Jia
  */
 public class JSPFileTests extends XmlSearchTestsBase
 {
@@ -49,9 +53,9 @@ public class JSPFileTests extends XmlSearchTestsBase
         return project;
     }
 
-    private IFile getViewJspFile() throws Exception
+    private IFile getJspFile(String fileName) throws Exception
     {
-        final IFile file =  CoreUtil.getDefaultDocrootFolder( getProject() ).getFile( "view.jsp" );
+        final IFile file =  CoreUtil.getDefaultDocrootFolder( getProject() ).getFile( fileName );
 
         if( file != null && file.exists() )
         {
@@ -87,7 +91,7 @@ public class JSPFileTests extends XmlSearchTestsBase
     // an example of testing quick fix
     protected void testMessageKeyQuickFix() throws Exception
     {
-        final IFile viewJspFile = getViewJspFile();
+        final IFile viewJspFile = getJspFile("view.jsp");
         assertNotNull( viewJspFile );
 
         final String elementName = "liferay-ui:message";
@@ -101,4 +105,102 @@ public class JSPFileTests extends XmlSearchTestsBase
 
         verifyQuickFix( viewJspFile, markerType, exceptedMessageRegex, AddResourceKeyMarkerResolution.class );
     }
+
+    @Test
+    public void testTagLiferayPortletParam() throws Exception
+    {
+        if( shouldSkipBundleTests() )
+        {
+            return;
+        }
+
+        final IFile jspFile = getJspFile( "test-liferay-portlet-param.jsp" );
+        assertNotNull( jspFile );
+
+        final String liferayPortletParamElementName = "liferay-portlet:param";
+
+        String[][] paramsArray =
+            { { "<%= ActionRequest.ACTION_NAME %>", "sayHello1", "Type \"sayHello1\" not found.", "true" },
+                { "javax.portlet.action", "sayHello2", "Type \"sayHello2\" not found.", "true" },
+                { "bookName", "liferay-in-action", "", "false" } };
+
+        for( String[] params : paramsArray )
+        {
+            testTagParamValidation(
+                jspFile, liferayPortletParamElementName, params[0], params[1], params[2], Boolean.valueOf( params[3] ) );
+        }
+    }
+
+    @Test
+    public void testTagParam() throws Exception
+    {
+        if( shouldSkipBundleTests() )
+        {
+            return;
+        }
+
+        final IFile jspFile = getJspFile( "test-param.jsp" );
+        assertNotNull( jspFile );
+
+        final String paramElementName = "param";
+
+        String[][] paramsArray =
+            { { "<%= ActionRequest.ACTION_NAME %>", "sayHello3", "", "false" },
+                { "javax.portlet.action", "sayHello4", "", "false" }, { "bookName", "liferay-in-action", "", "false" } };
+
+        for( String[] params : paramsArray )
+        {
+            testTagParamValidation(
+                jspFile, paramElementName, params[0], params[1], params[2], Boolean.valueOf( params[3] ) );
+        }
+    }
+
+    @Test
+    public void testTagPortletParam() throws Exception
+    {
+        if( shouldSkipBundleTests() )
+        {
+            return;
+        }
+
+        final IFile jspFile = getJspFile( "test-portlet-param.jsp" );
+        assertNotNull( jspFile );
+
+        final String portletParamElementName = "portlet:param";
+
+        String[][] paramsArray =
+            { { "<%= ActionRequest.ACTION_NAME %>", "sayHello5", "Type \"sayHello5\" not found.", "true" },
+                { "javax.portlet.action", "sayHello6", "Type \"sayHello6\" not found.", "true" },
+                { "bookName", "liferay-in-action", "", "false" } };
+
+        for( String[] params : paramsArray )
+        {
+            testTagParamValidation(
+                jspFile, portletParamElementName, params[0], params[1], params[2], Boolean.valueOf( params[3] ) );
+        }
+    }
+
+    protected void testTagParamValidation(
+        IFile file, String tagName, String nameAttr, String valueAttr, String exceptedMessageRegex,
+        boolean exceptedFindWarning ) throws Exception
+    {
+        final String markerType = XMLSearchConstants.LIFERAY_JSP_MARKER_ID;
+
+        setAttrValue( file, tagName, "name", nameAttr );
+        setAttrValue( file, tagName, "value", valueAttr );
+
+        buildAndValidate( file );
+
+        if( exceptedFindWarning )
+        {
+            assertEquals( true, checkMarkerByMessage( file, markerType, exceptedMessageRegex, true ) );
+        }
+        else
+        {
+            final IMarker[] markers = file.findMarkers( markerType, false, IResource.DEPTH_ZERO );
+
+            assertEquals( 0, markers.length );
+        }
+    }
+
 }
