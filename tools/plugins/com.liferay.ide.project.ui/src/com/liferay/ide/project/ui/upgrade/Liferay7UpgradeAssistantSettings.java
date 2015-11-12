a7b9fe96f883bd475b139ca89846a211ac14d49c
Lovett Li <lovett.li@liferay.com>
2015-11-12 15:49:50 +0800

    IDE-2139 Add JSON file store information of Liferay Portal setup

---
+++ tools/plugins/com.liferay.ide.project.ui/src/com/liferay/ide/project/ui/upgrade/Liferay7UpgradeAssistantSettings.java
@@ -0,0 +1,13 @@
+package com.liferay.ide.project.ui.upgrade;
+
+/**
+ * @author Lovett Li
+ */
+public class Liferay7UpgradeAssistantSettings
+{
+
+    public Liferay7UpgradeAssistantSettings()
+    {
+    }
+
+}
