5efae2617d6bdae3a7c7ea639c2190344ee4e326
Lovett Li <lovett.li@liferay.com>
2016-05-13 14:43:17 +0800

    Related with STUDIO-438

---
+++ tools/plugins/com.liferay.ide.gradle.core/src/com/liferay/ide/gradle/core/workspace/InitLiferayWorkspaceOp.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ *******************************************************************************/
+package com.liferay.ide.gradle.core.workspace;
+
+import org.eclipse.sapphire.ElementType;
+import org.eclipse.sapphire.Type;
+import org.eclipse.sapphire.Value;
+import org.eclipse.sapphire.ValueProperty;
+import org.eclipse.sapphire.modeling.Path;
+import org.eclipse.sapphire.modeling.annotations.AbsolutePath;
+import org.eclipse.sapphire.modeling.annotations.Enablement;
+import org.eclipse.sapphire.modeling.annotations.FileSystemResourceType;
+import org.eclipse.sapphire.modeling.annotations.Label;
+import org.eclipse.sapphire.modeling.annotations.Service;
+import org.eclipse.sapphire.modeling.annotations.ValidFileSystemResourceType;
+
+/**
+ * @author Lovett Li
+ */
+public interface InitLiferayWorkspaceOp extends NewLiferayWorkspaceOp
+{
+    ElementType TYPE = new ElementType( InitLiferayWorkspaceOp.class );
+
+    // *** Init LiferayWorkspace Location ***
+
+    @Type( base = Path.class )
+    @AbsolutePath
+    @Enablement( expr = "${ UseDefaultLocation == 'false' }" )
+    @ValidFileSystemResourceType( FileSystemResourceType.FOLDER )
+    @Label( standard = "liferay workspace location" )
+    @Service( impl = WorkspaceLocationValidationService.class )
+    ValueProperty PROP_LOCATION = new ValueProperty( TYPE, "Location" );
+
+    Value<Path> getLocation();
+    void setLocation( String value );
+    void setLocation( Path value );
+
+}
