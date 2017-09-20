685f2aa492bfa9641ee03ae9ac49c3ac61badcf6
Simon Jiang <simon.jiang@liferay.com>
2017-09-20 11:05:35 +0800

    IDE-3343 should support to start muti-servers at the same time

--- tools/plugins/com.liferay.ide.server.ui/src/com/liferay/ide/server/ui/portal/PortalServerWizard.java
+++ tools/plugins/com.liferay.ide.server.ui/src/com/liferay/ide/server/ui/portal/PortalServerWizard.java
@@ -15,10 +15,29 @@
 /**
  *
  */
+
 package com.liferay.ide.server.ui.portal;
 
-import org.eclipse.wst.server.ui.wizard.WizardFragment;
+import com.liferay.ide.server.core.LiferayServerCore;
+import com.liferay.ide.server.core.portal.LiferayServerPort;
+import com.liferay.ide.server.core.portal.PortalServerDelegate;
+import com.liferay.ide.server.ui.LiferayServerUI;
+import com.liferay.ide.server.util.ServerUtil;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.wst.server.core.IRuntime;
+import org.eclipse.wst.server.core.IServer;
+import org.eclipse.wst.server.core.IServerWorkingCopy;
+import org.eclipse.wst.server.core.TaskModel;
+import org.eclipse.wst.server.ui.wizard.WizardFragment;
 
 /**
  * @author Gregory Amerson
@@ -31,4 +50,51 @@ public class PortalServerWizard extends WizardFragment
         super();
     }
 
+    public void exit()
+    {
+        IServerWorkingCopy serverWorkingCopy = getServerWorkingCopy();
+        IRuntime runtime = serverWorkingCopy.getRuntime();
+        IServer[] servers = ServerUtil.getServersForRuntime( runtime );
+
+        if( servers.length > 1 )
+        {
+            LiferayServerUI.logWarning(
+                "The runtime selected already has server(s), you shouldn't make multiple servers point to the same runtime if you want to launch multiple servers in IDE at one time" );
+        }
+    }
+
+    private void saveDefaultPorsts()
+    {
+        IPath defaultPortsJson = LiferayServerCore.getDefault().getStateLocation().append(
+            getServerWorkingCopy().getId().replace( " ", "_" ) + "_default_ports.json" );
+
+        if( !defaultPortsJson.toFile().exists() )
+        {
+            try
+            {
+                PortalServerDelegate portalSeverDelgate = (PortalServerDelegate) getServerWorkingCopy().loadAdapter(
+                    PortalServerDelegate.class, new NullProgressMonitor() );
+                File defaultPortsFile = new File( defaultPortsJson.toOSString() );
+                List<LiferayServerPort> liferayServerPorts = portalSeverDelgate.getLiferayServerPorts();
+                final ObjectMapper mapper = new ObjectMapper();
+                mapper.writeValue( defaultPortsFile, liferayServerPorts );
+            }
+            catch( IOException e )
+            {
+                LiferayServerUI.logError( "Failed to save server default ports inforamion", e );
+            }
+        }
+    }
+
+    @Override
+    public void performFinish( IProgressMonitor monitor ) throws CoreException
+    {
+        super.performFinish( monitor );
+        saveDefaultPorsts();
+    }
+
+    protected IServerWorkingCopy getServerWorkingCopy()
+    {
+        return (IServerWorkingCopy) getTaskModel().getObject( TaskModel.TASK_SERVER );
+    }
 }
