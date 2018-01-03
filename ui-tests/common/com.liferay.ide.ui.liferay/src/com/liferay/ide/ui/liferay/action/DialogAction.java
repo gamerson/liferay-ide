33b8674ef57c5ebb2889408edbf8efd18d004d7b
Terry Jia <terry.jia@liferay.com>
2018-01-03 21:34:35 +0800

    IDE-3610 sf

--- ui-tests/common/com.liferay.ide.ui.liferay/src/com/liferay/ide/ui/liferay/action/DialogAction.java
+++ ui-tests/common/com.liferay.ide.ui.liferay/src/com/liferay/ide/ui/liferay/action/DialogAction.java
@@ -24,6 +24,7 @@ import com.liferay.ide.ui.swtbot.eclipse.page.TextTableDialog;
 import com.liferay.ide.ui.swtbot.eclipse.page.TreeDialog;
 import com.liferay.ide.ui.swtbot.page.Button;
 import com.liferay.ide.ui.swtbot.page.Dialog;
+import com.liferay.ide.ui.swtbot.page.Table;
 import com.liferay.ide.ui.swtbot.util.CoreUtil;
 
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
@@ -104,8 +105,8 @@ public class DialogAction extends UIAction {
 
 	public class AvailableSoftwareSitesDialogAction {
 
-		public boolean checkLiferayIdeSiteExist() {
-			return _availableSoftwareSitesPreferencesDialog.getSites().containsText(LIFERAY_IDE_STABLE_RELEASES);
+		public Table getSites() {
+			return _availableSoftwareSitesPreferencesDialog.getSites();
 		}
 
 		private final AvailableSoftwareSitesPreferencesDialog _availableSoftwareSitesPreferencesDialog =
