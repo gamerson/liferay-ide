f15df25dcb4d026ed433d5df8321b04f7c943870
Terry Jia <terry.jia@liferay.com>
2018-01-20 07:52:12 +0800

    Apply portal code style for com.liferay.ide.layouttpl.core.tests

--- tools/tests/com.liferay.ide.layouttpl.core.tests/src/com/liferay/ide/layouttpl/core/tests/LayoutTplTestsBootstrap.java
+++ tools/tests/com.liferay.ide.layouttpl.core.tests/src/com/liferay/ide/layouttpl/core/tests/LayoutTplTestsBootstrap.java
@@ -1,6 +1,18 @@
-package com.liferay.ide.layouttpl.core.tests;
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
-import static org.junit.Assert.assertEquals;
+package com.liferay.ide.layouttpl.core.tests;
 
 import com.liferay.ide.layouttpl.core.model.LayoutTplElement;
 import com.liferay.ide.layouttpl.core.model.PortletColumnElement;
@@ -8,228 +20,278 @@ import com.liferay.ide.layouttpl.core.model.PortletLayoutElement;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.sapphire.ElementList;
+import org.eclipse.sapphire.Value;
 import org.eclipse.sapphire.services.ValidationService;
-import org.junit.Test;
 
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * @author Kuo Zhang
- *
  */
-public class LayoutTplTestsBootstrap extends LayoutTplCoreTests
-{
+public class LayoutTplTestsBootstrap extends LayoutTplCoreTests {
 
-    protected LayoutTplElement createModel_132_nest( boolean isBootstrapStyle, String className, boolean is62)
-    {
-        final LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
-        layoutTpl.setBootstrapStyle( isBootstrapStyle );
-        layoutTpl.setClassName( className );
-        layoutTpl.setIs62( is62 );
+	@Test
+	public void evalTemplateFromChangedModel_1_3_2_nest_columns() throws Exception {
+		IFile refTplFile = getFileFromTplName("1_3_2_nest_changed_columns.tpl");
 
-        final PortletLayoutElement row1 = layoutTpl.getPortletLayouts().insert();
+		String className = convertToTplClassName("1_3_2_nest_changed_columns.tpl");
 
-        final PortletColumnElement column11 = row1.getPortletColumns().insert();
-        column11.setWeight( 12 );
+		LayoutTplElement layoutTpl = createModel_132_nest(isBootstrapStyle(), className, is62());
 
-        final PortletLayoutElement row2 = layoutTpl.getPortletLayouts().insert();
+		PortletLayoutElement row1 = (PortletLayoutElement)layoutTpl.getPortletLayouts().get(0);
+		PortletLayoutElement row2 = (PortletLayoutElement)layoutTpl.getPortletLayouts().get(1);
+		PortletLayoutElement row3 = (PortletLayoutElement)layoutTpl.getPortletLayouts().get(2);
 
-        final PortletColumnElement column21 = row2.getPortletColumns().insert();
-        column21.setWeight( 4 );
+		PortletColumnElement element = row3.getPortletColumns().get(0);
 
-        final PortletColumnElement column22 = row2.getPortletColumns().insert();
-        column22.setWeight( 4 );
+		PortletLayoutElement row311 = element.getPortletLayouts().get(0);
 
-        final PortletColumnElement column23 = row2.getPortletColumns().insert();
-        column23.setWeight( 4 );
+		element = row3.getPortletColumns().get(0);
 
-        final PortletLayoutElement row3 = layoutTpl.getPortletLayouts().insert();
+		PortletLayoutElement row312 = element.getPortletLayouts().get(1);
 
-        final PortletColumnElement column31 = row3.getPortletColumns().insert();
-        column31.setWeight( 8 );
+		element = row312.getPortletColumns().get(1);
 
-        final PortletLayoutElement row311 = column31.getPortletLayouts().insert();
+		PortletLayoutElement row31221 = element.getPortletLayouts().get(0);
 
-        final PortletColumnElement column3111 = row311.getPortletColumns().insert();
-        column3111.setWeight( 12 );
+		row1.getPortletColumns().remove(row1.getPortletColumns().get(0));
 
-        final PortletLayoutElement row312 = column31.getPortletLayouts().insert();
+		layoutTpl.getPortletLayouts().remove(row1);
 
-        final PortletColumnElement column3121 = row312.getPortletColumns().insert();
-        column3121.setWeight( 6 );
+		PortletColumnElement insertedColumn = row311.getPortletColumns().insert();
 
-        final PortletColumnElement column3122 = row312.getPortletColumns().insert();
-        column3122.setWeight( 6 );
+		insertedColumn.setWeight(3);
 
-        final PortletLayoutElement row31221 = column3122.getPortletLayouts().insert();
+		element = row311.getPortletColumns().get(0);
 
-        final PortletColumnElement column312211 = row31221.getPortletColumns().insert();
-        column312211.setWeight( 12 );
+		element.setWeight(9);
 
-        final PortletColumnElement column32 = row3.getPortletColumns().insert();
-        column32.setWeight( 4 );
+		row2.getPortletColumns().remove(row2.getPortletColumns().get(0));
 
-        return layoutTpl;
-    }
+		element = row2.getPortletColumns().get(0);
 
-    @Test
-    public void evalTemplateFromChangedModel_1_3_2_nest_columns() throws Exception
-    {
-        IFile refTplFile = getFileFromTplName( "1_3_2_nest_changed_columns.tpl" );
+		element.setWeight(8);
 
-        final String className = convertToTplClassName( "1_3_2_nest_changed_columns.tpl" );
-        final LayoutTplElement layoutTpl = createModel_132_nest( isBootstrapStyle(), className, is62());
+		insertedColumn = row31221.getPortletColumns().insert();
 
-        final PortletLayoutElement row1 = (PortletLayoutElement) layoutTpl.getPortletLayouts().get( 0 );
-        final PortletLayoutElement row2 = (PortletLayoutElement) layoutTpl.getPortletLayouts().get( 1 );
-        final PortletLayoutElement row3 = (PortletLayoutElement) layoutTpl.getPortletLayouts().get( 2 );
+		insertedColumn.setWeight(2);
 
-        final PortletLayoutElement row311 = row3.getPortletColumns().get( 0 ).getPortletLayouts().get( 0 );
+		element = row31221.getPortletColumns().get(0);
 
-        final PortletLayoutElement row312 = row3.getPortletColumns().get( 0 ).getPortletLayouts().get( 1 );
+		element.setWeight(10);
 
-        final PortletLayoutElement row31221 = row312.getPortletColumns().get( 1 ).getPortletLayouts().get( 0 );
+		evalModelWithFile(refTplFile, layoutTpl);
+	}
 
-        row1.getPortletColumns().remove( row1.getPortletColumns().get( 0 ) );
-        layoutTpl.getPortletLayouts().remove( row1 );
+	@Test
+	public void testPorteltColumnWeightValidationService() {
+		LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
 
-        PortletColumnElement insertedColumn = row311.getPortletColumns().insert();
-        insertedColumn.setWeight( 3 );
-        row311.getPortletColumns().get( 0 ).setWeight( 9 );
+		layoutTpl.setBootstrapStyle(true);
 
-        row2.getPortletColumns().remove( row2.getPortletColumns().get( 0 ) );
-        row2.getPortletColumns().get( 0 ).setWeight( 8 );
+		PortletLayoutElement row = layoutTpl.getPortletLayouts().insert();
 
-        insertedColumn = row31221.getPortletColumns().insert();
-        insertedColumn.setWeight( 2 );
-        row31221.getPortletColumns().get( 0 ).setWeight( 10 );
+		PortletColumnElement column = row.getPortletColumns().insert();
 
-        evalModelWithFile( refTplFile, layoutTpl );
-    }
+		ValidationService validationService = column.getWeight().service(ValidationService.class);
 
-    @Override
-    protected String getFilesPrefix()
-    {
-        return "bootstrap/files/";
-    }
+		column.setWeight(0);
 
-    @Override
-    protected boolean isBootstrapStyle()
-    {
-        return true;
-    }
+		Assert.assertEquals(
+			"The weight value is invalid, should be in (0, 12]", validationService.validation().message());
 
-    @Override
-    protected boolean is62()
-    {
-        return true;
-    }
+		column.setWeight(-1);
+		Assert.assertEquals(
+			"The weight value is invalid, should be in (0, 12]", validationService.validation().message());
 
-    @Test
-    public void testPorteltColumnWeightValidationService()
-    {
-        final LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
-        layoutTpl.setBootstrapStyle( true );
+		column.setWeight(13);
+		Assert.assertEquals(
+			"The weight value is invalid, should be in (0, 12]", validationService.validation().message());
 
-        final PortletLayoutElement row = layoutTpl.getPortletLayouts().insert();
-        final PortletColumnElement column = row.getPortletColumns().insert();
+		column.setWeight(6);
+		Assert.assertEquals("ok", validationService.validation().message());
+	}
 
-        final ValidationService validationService = column.getWeight().service( ValidationService.class );
+	@Test
+	public void testPortletColumnFullWeightDefaultValueService() throws Exception {
+		LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
 
-        column.setWeight( 0 );
-        assertEquals( "The weight value is invalid, should be in (0, 12]", validationService.validation().message() );
+		layoutTpl.setBootstrapStyle(true);
 
-        column.setWeight( -1 );
-        assertEquals( "The weight value is invalid, should be in (0, 12]", validationService.validation().message() );
+		PortletLayoutElement e = layoutTpl.getPortletLayouts().insert();
 
-        column.setWeight( 13 );
-        assertEquals( "The weight value is invalid, should be in (0, 12]", validationService.validation().message() );
+		PortletColumnElement column = e.getPortletColumns().insert();
 
-        column.setWeight( 6 );
-        assertEquals( "ok", validationService.validation().message() );
-    }
+		Value<Integer> w = column.getFullWeight();
 
-    @Test
-    public void testPortletColumnFullWeightDefaultValueService() throws Exception
-    {
-        final LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
+		Assert.assertEquals(12, w.content(true).intValue());
+	}
 
-        layoutTpl.setBootstrapStyle( true );
+	@Test
+	public void testPortletColumnsValidationSerive() {
+		LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
 
-        final PortletColumnElement column = layoutTpl.getPortletLayouts().insert().getPortletColumns().insert();
+		layoutTpl.setBootstrapStyle(true);
 
-        assertEquals( 12, column.getFullWeight().content( true ).intValue() );
-    }
+		PortletLayoutElement row = layoutTpl.getPortletLayouts().insert();
 
-    // test sum of column weights
-    @Test
-    public void testPortletColumnsValidationSerive()
-    {
-        final LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
-        layoutTpl.setBootstrapStyle( true );
+		ElementList<PortletColumnElement> columns = row.getPortletColumns();
 
-        final PortletLayoutElement row = layoutTpl.getPortletLayouts().insert();
-        final ElementList<PortletColumnElement> columns = row.getPortletColumns();
-        final PortletColumnElement column = columns.insert();
+		PortletColumnElement column = columns.insert();
 
-        final ValidationService validationService = columns.service( ValidationService.class );
+		ValidationService validationService = columns.service(ValidationService.class);
 
-        assertEquals( "ok", validationService.validation().message() );
+		Assert.assertEquals("ok", validationService.validation().message());
 
-        column.setWeight( 0 );
-        assertEquals( "The sum of weight of columns should be: 12", validationService.validation().message() );
+		column.setWeight(0);
+		Assert.assertEquals("The sum of weight of columns should be: 12", validationService.validation().message());
 
-        column.setWeight( -1 );
-        assertEquals( "The sum of weight of columns should be: 12", validationService.validation().message() );
+		column.setWeight(-1);
+		Assert.assertEquals("The sum of weight of columns should be: 12", validationService.validation().message());
 
-        column.setWeight( 6 );
-        assertEquals( "The sum of weight of columns should be: 12", validationService.validation().message() );
+		column.setWeight(6);
+		Assert.assertEquals("The sum of weight of columns should be: 12", validationService.validation().message());
 
-        column.setWeight( 13 );
-        assertEquals( "The sum of weight of columns should be: 12", validationService.validation().message() );
+		column.setWeight(13);
+		Assert.assertEquals("The sum of weight of columns should be: 12", validationService.validation().message());
 
-        column.setWeight( 12 );
-        assertEquals( "ok", validationService.validation().message() );
-    }
+		column.setWeight(12);
+		Assert.assertEquals("ok", validationService.validation().message());
+	}
 
-    @Test
-    public void testPortletColumnWeightInitialValueService() throws Exception
-    {
-        final LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
-        layoutTpl.setBootstrapStyle( true );
+	@Test
+	public void testPortletColumnWeightInitialValueService() throws Exception {
+		LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
 
-        final PortletLayoutElement row = layoutTpl.getPortletLayouts().insert();
-        final ElementList<PortletColumnElement> columns = row.getPortletColumns();
+		layoutTpl.setBootstrapStyle(true);
 
-        columns.insert();
-        columns.insert();
-        columns.insert();
-        columns.insert();
+		PortletLayoutElement row = layoutTpl.getPortletLayouts().insert();
 
-        assertEquals( 6, columns.get( 0 ).getWeight().content().intValue() );
-        assertEquals( 3, columns.get( 1 ).getWeight().content().intValue() );
-        assertEquals( 2, columns.get( 2 ).getWeight().content().intValue() );
-        assertEquals( 1, columns.get( 3 ).getWeight().content().intValue() );
+		ElementList<PortletColumnElement> columns = row.getPortletColumns();
 
-        columns.get( 0 ).setWeight( 2 );
-        columns.get( 1 ).setWeight( 2 );
-        columns.get( 2 ).setWeight( 2 );
-        columns.get( 3 ).setWeight( 2 );
+		columns.insert();
+		columns.insert();
+		columns.insert();
+		columns.insert();
 
-        columns.insert();
-        assertEquals( 4, columns.get( 4 ).getWeight().content().intValue() );
-    }
+		Value<Integer> weight = columns.get(0).getWeight();
 
-    @Test
-    public void testPortletLayoutClassNameDefaultValueService() throws Exception
-    {
-        final LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
+		Assert.assertEquals(6, weight.content().intValue());
 
-        layoutTpl.setBootstrapStyle( true );
-        layoutTpl.setIs62( true );
+		weight = columns.get(1).getWeight();
 
-        final PortletLayoutElement row = layoutTpl.getPortletLayouts().insert();
+		Assert.assertEquals(3, weight.content().intValue());
 
-        assertEquals( "portlet-layout row-fluid", row.getClassName().content( true ) );
-    }
+		weight = columns.get(2).getWeight();
+
+		Assert.assertEquals(2, weight.content().intValue());
+
+		weight = columns.get(3).getWeight();
+
+		Assert.assertEquals(1, weight.content().intValue());
+
+		columns.get(0).setWeight(2);
+		columns.get(1).setWeight(2);
+		columns.get(2).setWeight(2);
+		columns.get(3).setWeight(2);
+
+		columns.insert();
+
+		weight = columns.get(4).getWeight();
+
+		Assert.assertEquals(4, weight.content().intValue());
+	}
+
+	@Test
+	public void testPortletLayoutClassNameDefaultValueService() throws Exception {
+		LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
+
+		layoutTpl.setBootstrapStyle(true);
+		layoutTpl.setIs62(true);
+
+		PortletLayoutElement row = layoutTpl.getPortletLayouts().insert();
+
+		Assert.assertEquals("portlet-layout row-fluid", row.getClassName().content(true));
+	}
+
+	protected LayoutTplElement createModel_132_nest(boolean bootstrapStyle, String className, boolean is62) {
+		LayoutTplElement layoutTpl = LayoutTplElement.TYPE.instantiate();
+
+		layoutTpl.setBootstrapStyle(bootstrapStyle);
+		layoutTpl.setClassName(className);
+		layoutTpl.setIs62(is62);
+
+		PortletLayoutElement row1 = layoutTpl.getPortletLayouts().insert();
+
+		PortletColumnElement column11 = row1.getPortletColumns().insert();
+
+		column11.setWeight(12);
+
+		PortletLayoutElement row2 = layoutTpl.getPortletLayouts().insert();
+
+		PortletColumnElement column21 = row2.getPortletColumns().insert();
+
+		column21.setWeight(4);
+
+		PortletColumnElement column22 = row2.getPortletColumns().insert();
+
+		column22.setWeight(4);
+
+		PortletColumnElement column23 = row2.getPortletColumns().insert();
+
+		column23.setWeight(4);
+
+		PortletLayoutElement row3 = layoutTpl.getPortletLayouts().insert();
+
+		PortletColumnElement column31 = row3.getPortletColumns().insert();
+
+		column31.setWeight(8);
+
+		PortletLayoutElement row311 = column31.getPortletLayouts().insert();
+
+		PortletColumnElement column3111 = row311.getPortletColumns().insert();
+
+		column3111.setWeight(12);
+
+		PortletLayoutElement row312 = column31.getPortletLayouts().insert();
+
+		PortletColumnElement column3121 = row312.getPortletColumns().insert();
+
+		column3121.setWeight(6);
+
+		PortletColumnElement column3122 = row312.getPortletColumns().insert();
+
+		column3122.setWeight(6);
+
+		PortletLayoutElement row31221 = column3122.getPortletLayouts().insert();
+
+		PortletColumnElement column312211 = row31221.getPortletColumns().insert();
+
+		column312211.setWeight(12);
+
+		PortletColumnElement column32 = row3.getPortletColumns().insert();
+
+		column32.setWeight(4);
+
+		return layoutTpl;
+	}
+
+	// test sum of column weights
+
+	@Override
+	protected String getFilesPrefix() {
+		return "bootstrap/files/";
+	}
+
+	@Override
+	protected boolean is62() {
+		return true;
+	}
+
+	@Override
+	protected boolean isBootstrapStyle() {
+		return true;
+	}
 
 }
