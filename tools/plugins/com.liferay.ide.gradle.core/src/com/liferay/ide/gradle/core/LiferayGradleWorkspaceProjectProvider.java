6d4a58e1a0c3e7e9c37261d20773877667dc6466
Gregory Amerson <gregory.amerson@liferay.com>
2018-04-03 11:57:44 -0500

    IDE-3943 ignore workspace projects that can't resolve location

--- tools/plugins/com.liferay.ide.gradle.core/src/com/liferay/ide/gradle/core/LiferayGradleWorkspaceProjectProvider.java
+++ tools/plugins/com.liferay.ide.gradle.core/src/com/liferay/ide/gradle/core/LiferayGradleWorkspaceProjectProvider.java
@@ -158,9 +158,13 @@ public class LiferayGradleWorkspaceProjectProvider
 			LiferayWorkspaceUtil.getWorkspaceProject()
 		).filter(
 			workspaceProject -> {
-				IPath workspaceLocation = workspaceProject.getRawLocation();
+				IPath workspaceProjectLocation = workspaceProject.getRawLocation();
 
-				return workspaceLocation.isPrefixOf(liferayHome);
+				if (workspaceProjectLocation == null) {
+					return false;
+				}
+
+				return workspaceProjectLocation.isPrefixOf(liferayHome);
 			}
 		).map(
 			workspaceProject -> LiferayCore.create(IWorkspaceProject.class, workspaceProject)
