b8ed17c493a8eb1ccbbdfccd321fb3a29c2d8d07
Terry Jia <terry.jia@liferay.com>
2018-10-09 13:41:44 +0800

    IDE-4298 npe

--- tools/plugins/com.liferay.ide.gradle.core/src/com/liferay/ide/gradle/core/LiferayGradleWorkspaceProject.java
+++ tools/plugins/com.liferay.ide.gradle.core/src/com/liferay/ide/gradle/core/LiferayGradleWorkspaceProject.java
@@ -98,6 +98,8 @@ public class LiferayGradleWorkspaceProject extends LiferayWorkspaceProject {
 
 			stream.map(
 				IProject::getLocation
+			).filter(
+				location -> location != null
 			).map(
 				location -> _convertToModuleTaskPath(location)
 			).forEach(
