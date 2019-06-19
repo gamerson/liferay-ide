da6d2edadabd5f28c3072d6b292e8d638e0b195d
Terry Jia <terry.jia@liferay.com>
2019-06-19 13:58:07 +0800

    IDE-4110 get supported liferay version of project template from blade jar

--- tools/plugins/com.liferay.ide.project.core/src/com/liferay/ide/project/core/modules/ProjectTemplateNameValidationService.java
+++ tools/plugins/com.liferay.ide.project.core/src/com/liferay/ide/project/core/modules/ProjectTemplateNameValidationService.java
@@ -14,56 +14,127 @@
 
 package com.liferay.ide.project.core.modules;
 
-import com.liferay.ide.core.util.ListUtil;
+import aQute.bnd.version.Version;
+import aQute.bnd.version.VersionRange;
 
-import java.util.Set;
+import com.liferay.ide.core.util.FileUtil;
+import com.liferay.ide.core.util.SapphireContentAccessor;
+import com.liferay.ide.project.core.ProjectCore;
 
-import org.eclipse.sapphire.Event;
-import org.eclipse.sapphire.Listener;
-import org.eclipse.sapphire.Value;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.Enumeration;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+
+import org.apache.commons.io.IOUtils;
+
 import org.eclipse.sapphire.modeling.Status;
 import org.eclipse.sapphire.services.ValidationService;
 
 /**
  * @author Gregory Amerson
+ * @author Terry Jia
  */
-public class ProjectTemplateNameValidationService extends ValidationService {
+public class ProjectTemplateNameValidationService extends ValidationService implements SapphireContentAccessor {
 
 	@Override
 	protected Status compute() {
-		return _templateNameStatus;
-	}
-
-	@Override
-	protected void initValidationService() {
-		super.initValidationService();
+		Status retval = Status.createOkStatus();
 
 		NewLiferayModuleProjectOp op = context(NewLiferayModuleProjectOp.class);
 
-		Value<Object> value = op.property(NewLiferayModuleProjectOp.PROP_PROJECT_TEMPLATE_NAME);
+		String liferayVersion = get(op.getLiferayVersion());
 
-		ProjectTemplateNamePossibleValuesService pvs = value.service(ProjectTemplateNamePossibleValuesService.class);
+		String projectTemplateName = get(op.getProjectTemplateName());
 
-		Set<String> templateNames = pvs.values();
+		projectTemplateName = projectTemplateName.replace("-", ".");
 
-		if (ListUtil.isNotEmpty(templateNames)) {
-			_templateNameStatus = Status.createOkStatus();
-		}
-		else {
-			pvs.attach(
-				new Listener() {
+		try {
+			File bladeJar = FileUtil.getFile(BladeCLI.getBladeCLIPath());
 
-					@Override
-					public void handle(Event event) {
-						_templateNameStatus = Status.createOkStatus();
+			ZipFile zipFile = new ZipFile(bladeJar);
 
-						refresh();
+			Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+			while (entries.hasMoreElements()) {
+				ZipEntry entry = entries.nextElement();
+
+				String entryName = entry.getName();
+
+				if (entryName.endsWith(".jar") &&
+					entryName.startsWith("com.liferay.project.templates." + projectTemplateName)) {
+
+					try (InputStream in = zipFile.getInputStream(entry)) {
+						ProjectCore projectCore = ProjectCore.getDefault();
+
+						File stateFile = FileUtil.getFile(projectCore.getStateLocation());
+
+						File tempFile = new File(stateFile, entryName);
+
+						FileUtil.writeFileFromStream(tempFile, in);
+
+						ZipFile tempZipFile = new ZipFile(tempFile);
+
+						Enumeration<? extends ZipEntry> tempEntries = tempZipFile.entries();
+
+						while (tempEntries.hasMoreElements()) {
+							ZipEntry tempEntry = tempEntries.nextElement();
+
+							String tempEntryName = tempEntry.getName();
+
+							if (tempEntryName.equals("META-INF/MANIFEST.MF")) {
+								try (InputStream manifestInput = tempZipFile.getInputStream(tempEntry)) {
+									List<String> lines = IOUtils.readLines(manifestInput);
+
+									for (String line : lines) {
+										String liferayVersionString = "Liferay-Versions:";
+
+										if (line.startsWith(liferayVersionString)) {
+											String versionRangeValue = line.substring(liferayVersionString.length());
+
+											VersionRange versionRange = new VersionRange(versionRangeValue);
+
+											boolean include = versionRange.includes(new Version(liferayVersion));
+
+											if (!include) {
+												retval = Status.createErrorStatus(
+													"Specified Liferay version is invaild. Must be in range " +
+														versionRangeValue);
+											}
+
+											break;
+										}
+									}
+								}
+
+								break;
+							}
+						}
+
+						tempZipFile.close();
+
+						tempFile.delete();
 					}
 
-				});
-		}
-	}
+					break;
+				}
+			}
 
-	private Status _templateNameStatus = Status.createErrorStatus("Downloading templates, please wait...");
+			zipFile.close();
+		}
+		catch (BladeCLIException bclie) {
+		}
+		catch (ZipException ze) {
+		}
+		catch (IOException ioe) {
+		}
+
+		return retval;
+	}
 
 }
