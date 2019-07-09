4cecf7361fe60c449b5961360ccba6dcd81c3823
Terry Jia <terry.jia@liferay.com>
2019-07-09 15:06:30 +0800

    update copyright

--- language-server/liferay-vscode-lsp/src/extension.ts
+++ language-server/liferay-vscode-lsp/src/extension.ts
@@ -1,7 +1,16 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
 import * as net from 'net';
 import * as child_process from "child_process";
