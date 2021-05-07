5924a978e3fb855d2fd3608c51fb0cd38d2fdb84
Ashley Yuan <yanan.yuan@liferay.com>
2021-05-07 20:05:36 +0800

    IDE-4965 upload files to linode instead of to bintray

--- build/osgi-bundle-repository/deployToBintray.sh
+++ build/osgi-bundle-repository/deployToBintray.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-# Deploys the generated p2 repository to Bintray.
+# Deprecated: Deploys the generated p2 repository to Bintray.
 #
-# @see https://bintray.com/docs/api/#_upload_content
+# Deprecated: @see https://bintray.com/docs/api/#_upload_content
+#
+# Deploys the generated p2 repository to Linode
 
-if [ $# != 5 ]
+echo "please make sure your s3cmd has been configured"
+
+if [ $# != 4 ]
 then
-  echo "Usage: $0 repo_path bt_owner bt_repo bt_package bt_version"
+  echo "Usage: $0 repo_path bt_repo bt_package bt_version"
   exit 1
 fi
 
 REPO_PATH=$1
-BT_OWNER=$2
-BT_REPO=$3
-BT_PACKAGE=$4
-BT_VERSION=$5
+BT_REPO=$2
+BT_PACKAGE=$3
+BT_VERSION=$4
 
-echo "creating version $BT_VERSION ..."
-curl -X POST -u $BINTRAY_USER:$BINTRAY_API_KEY -H "Content-Type: application/json" -d "{\"name\":\"$BT_VERSION\",\"vcs_tag\":\"$BT_VERSION\",\"desc\":\"Created by Travis CI\"}" "https://api.bintray.com/packages/$BT_OWNER/$BT_REPO/$BT_PACKAGE/versions"
+echo "uploading to version $BT_VERSION ..."
+s3cmd --no-mime-magic --acl-public --delete-removed --delete-after sync $REPO_PATH/ s3://devtools-s3.liferay.com/$BT_REPO/$BT_PACKAGE/$BT_VERSION/
 echo
-cd $REPO_PATH
-for F in artifacts.* content.* plugins/* features/*
-do
-  echo "deploying $F ..."
-  curl -X PUT -T $F -u $BINTRAY_USER:$BINTRAY_API_KEY "https://api.bintray.com/content/$BT_OWNER/$BT_REPO/$BT_PACKAGE/$BT_VERSION/$F;bt_package=$BT_PACKAGE;bt_version=$BT_VERSION;publish=1"
-  echo
-done
