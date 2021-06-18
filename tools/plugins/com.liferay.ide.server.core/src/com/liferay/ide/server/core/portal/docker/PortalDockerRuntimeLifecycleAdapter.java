db8554f3f685dbcb4a4692609f4c67924782f616
Ethan Sun <ethan.sun@liferay.com>
2021-06-18 14:49:04 +0800

    IDE-4712 fixup suggestion
    Conflicts:
    	tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/docker/PortalDockerRuntimeLifecycleAdapter.java
    	tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/docker/PortalDockerServerLifecycleAdapter.java

---
+++ tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/docker/PortalDockerRuntimeLifecycleAdapter.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.server.core.portal.docker;
+
+import com.liferay.ide.core.workspace.LiferayWorkspaceUtil;
+import com.liferay.ide.server.core.LiferayServerCore;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.wst.server.core.IRuntime;
+import org.eclipse.wst.server.core.util.RuntimeLifecycleAdapter;
+
+/**
+ * @author Simon Jiang
+ * @author Ethan Sun
+ */
+public class PortalDockerRuntimeLifecycleAdapter extends RuntimeLifecycleAdapter {
+
+	@Override
+	public void runtimeRemoved(IRuntime runtime) {
+		PortalDockerRuntime dockerRuntime = (PortalDockerRuntime)runtime.loadAdapter(PortalDockerRuntime.class, null);
+
+		if (dockerRuntime == null) {
+			return;
+		}
+
+		IProject project = LiferayWorkspaceUtil.getWorkspaceProject();
+
+		Job cleanDockerImageJob = new Job(project.getName() + " - cleanDockerImage") {
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				IDockerServer dockerServer = LiferayServerCore.getDockerServer();
+
+				if (dockerServer == null) {
+					return LiferayServerCore.createErrorStatus("Failed to get docker server");
+				}
+
+				try {
+					dockerServer.cleanDockerImage(monitor);
+				}
+				catch (Exception e) {
+					LiferayServerCore.logError("Failed to remove runtime", e);
+				}
+
+				return Status.OK_STATUS;
+			}
+
+		};
+
+		cleanDockerImageJob.schedule();
+	}
+
+}
---
+++ tools/plugins/com.liferay.ide.server.core/src/com/liferay/ide/server/core/portal/docker/PortalDockerServerLifecycleAdapter.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.ide.server.core.portal.docker;
+
+import com.liferay.ide.core.workspace.LiferayWorkspaceUtil;
+import com.liferay.ide.server.core.LiferayServerCore;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.wst.server.core.IServer;
+import org.eclipse.wst.server.core.util.ServerLifecycleAdapter;
+
+/**
+ * @author Simon Jiang
+ * @author Ethan Sun
+ */
+public class PortalDockerServerLifecycleAdapter extends ServerLifecycleAdapter {
+
+	@Override
+	public void serverRemoved(IServer server) {
+		PortalDockerServer portalDockerServer = (PortalDockerServer)server.loadAdapter(PortalDockerServer.class, null);
+
+		if (portalDockerServer == null) {
+			return;
+		}
+
+		IProject project = LiferayWorkspaceUtil.getWorkspaceProject();
+
+		Job removeDockerContainerJob = new Job(project.getName() + " - removeDockerContainer") {
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				IDockerServer dockerServer = LiferayServerCore.getDockerServer();
+
+				if (dockerServer == null) {
+					return LiferayServerCore.createErrorStatus("Failed to get docker supporter");
+				}
+
+				try {
+					dockerServer.removeDockerContainer(monitor);
+				}
+				catch (Exception e) {
+					LiferayServerCore.logError("Failed to remove server", e);
+				}
+
+				return Status.OK_STATUS;
+			}
+
+		};
+
+		removeDockerContainerJob.schedule();
+	}
+
+}
